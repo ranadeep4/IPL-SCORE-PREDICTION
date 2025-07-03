4bed53137acc22d52ff4df517ffc5e4424a6d20d
ranadeep4 <dema.ranadeepreddy2004@gmail.com>
2025-07-03 06:01:57 +0530

    updated app.py

--- app/app.py
+++ app/app.py
@@ -110,4 +110,4 @@ def index():
                            prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
